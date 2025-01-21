5df8170c0adb2f8bb342f026f3afb699d5e06cad
Miclasher <14122006mg@gmail.com>
2025-01-21 15:57:24 +0200

    Add project files.

---
+++ University/DataLayer/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace University.DataLayer.Models
+{
+    internal class Student
+    {
+        [Column("STUDENT_ID")]
+        public int Id { get; set; }
+        [Column("FIRST_NAME")]
+        public string FirstName { get; set; } = string.Empty;
+        [Column("LAST_NAME")]
+        public string LastName { get; set; } = string.Empty;
+        [Column("GROUP_ID")]
+        public Guid GroupId { get; set; }
+        public Group Group { get; set; } = null!;
+    }
+}
