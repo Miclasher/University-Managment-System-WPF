059e4c74df23d9de033b5c690db2ecce506d6930
Miclasher <14122006mg@gmail.com>
2025-01-23 13:16:05 +0200

    Sut up everything for creating CRUDs

--- University/Domain/ViewModels/StudentsViewModel.cs
+++ University/Domain/ViewModels/StudentsViewModel.cs
@@ -1,19 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using University.DataLayer;
+using University.DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Windows.Input;
+using University.Domain.Commands;
 
 namespace University.Domain.ViewModels
 {
     public class StudentsViewModel : BaseViewModel, INotifyPropertyChanged
     {
         private readonly UniversityContext _context;
+        private Student _selectedStudent = null!;
+        public Student SelectedStudent
+        {
+            get => _selectedStudent;
+            set
+            {
+                if (_selectedStudent != value)
+                {
+                    _selectedStudent = value;
+                    OnPropertyChanged(nameof(SelectedStudent));
+                }
+            }
+        }
+        public ObservableCollection<Student> Students { get; }
+        public ObservableCollection<Group> Groups { get; }
+        public ICommand AddStudentCommand { get; }
+        public ICommand DeleteStudentCommand { get; }
+        public ICommand SaveStudentCommand { get; }
+
         public StudentsViewModel(UniversityContext context)
         {
             _context = context;
+
+            Students = new ObservableCollection<Student>(_context.Students.Include(e => e.Group).ToList());
+            Groups = new ObservableCollection<Group>(_context.Groups.ToList());
+
+            AddStudentCommand = new RelayCommand(_ => AddStudent(), _ => true);
+            DeleteStudentCommand = new RelayCommand(_ => DeleteStudent(), _ => true);
+            SaveStudentCommand = new RelayCommand(_ => SaveStudent(), _ => true);
+        }
+
+        private void SaveStudent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DeleteStudent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddStudent()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- University/Domain/ViewModels/TeachersViewModel.cs
+++ University/Domain/ViewModels/TeachersViewModel.cs
@@ -1,19 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using University.DataLayer;
+using University.DataLayer.Models;
+using University.Domain.Commands;
 
 namespace University.Domain.ViewModels
 {
     public class TeachersViewModel : BaseViewModel, INotifyPropertyChanged
     {
         private readonly UniversityContext _context;
+        private Teacher _selectedTeacher = null!;
+        public Teacher SelectedTeacher
+        {
+            get => _selectedTeacher;
+            set
+            {
+                if (_selectedTeacher != value)
+                {
+                    _selectedTeacher = value;
+                    OnPropertyChanged(nameof(SelectedTeacher));
+                }
+            }
+        }
+        public ObservableCollection<Teacher> Teachers { get; }
+        public ICommand AddTeacherCommand { get; }
+        public ICommand DeleteTeacherCommand { get; }
+        public ICommand SaveTeacherCommand { get; }
         public TeachersViewModel(UniversityContext context)
         {
             _context = context;
+
+            Teachers = new ObservableCollection<Teacher>(_context.Teachers.ToList());
+
+            AddTeacherCommand = new RelayCommand(_ => AddTeacher(), _ => true);
+            DeleteTeacherCommand = new RelayCommand(_ => DeleteTeacher(), _ => true);
+            SaveTeacherCommand = new RelayCommand(_ => SaveTeacher(), _ => true);
+        }
+
+        private void SaveTeacher()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DeleteTeacher()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddTeacher()
+        {
+            throw new NotImplementedException();
         }
     }
 }
