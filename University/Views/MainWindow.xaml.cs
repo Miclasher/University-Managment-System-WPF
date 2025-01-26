1f4c1bc912419b5af03af7445607f3185ccc9d89
Miclasher <14122006mg@gmail.com>
2025-01-26 11:41:21 +0200

    Fix MainWindow view

---
+++ University/Views/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using University.Domain.ViewModels;
+using University.Views;
+
+namespace University
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(MainWindowViewModel model, MainViewModel pageModel)
+        {
+            InitializeComponent();
+
+            this.MainFrame.Navigate(new MainPage(pageModel));
+
+            DataContext = model;
+        }
+    }
+}
