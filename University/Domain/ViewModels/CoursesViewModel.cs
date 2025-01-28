7aed282caaf03f303c1be93d1c1ce5649284acb9
Miclasher <14122006mg@gmail.com>
2025-01-28 09:50:18 +0200

    Hide deatils if SelectedItem is null

--- University/Domain/ViewModels/CoursesViewModel.cs
+++ University/Domain/ViewModels/CoursesViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Runtime.InteropServices.Marshalling;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
